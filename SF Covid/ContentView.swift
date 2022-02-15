60c7ef159708620b82f4848e0c2869f9b1f9e355
Michael Critz <michael.critz@sony.com>
2022-02-14 16:34:59 -0800

    Initial Commit

---
+++ SF Covid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SF Covid
+//
+//  Created by Critz, Michael on 2/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SF Covid/SF_CovidApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_CovidApp.swift
+//  SF Covid
+//
+//  Created by Critz, Michael on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SF_CovidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
